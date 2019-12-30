4bfd1efa17b4f43b70273cc3cd057d7f8e57310f
Philippe Voinov <philippevoinov@gmail.com>
2019-12-30 12:54:43 +0100

    Restore old demo program

--- src/logic/providers/typescript/typescript-provider.ts
+++ src/logic/providers/typescript/typescript-provider.ts
@@ -10,43 +10,29 @@ interface WorkingSet {
 }
 export class TypescriptProvider {
   file = `
-if (something) {
-  console.log('bla')
-} else if (another) {
-  console.log('test')
-} else if (more) {
-  console.log('test!!!')
-} else {
-  console.log('test!!!')
-}
-if (something) {
-  console.log('bla')
-}
+  import {
+    Node,
+    DisplayInfoPriority,
+    LabelStyle,
+    DisplayInfo,
+  } from "../../tree/node";
+  import * as ts from "typescript";
+  export type Enchancer<T extends Node<ts.Node>> = (
+    node: T,
+  ) => {
+    displayInfo: DisplayInfo;
+  };
+  export const enchancers: {
+    [key: string]: Enchancer<Node<ts.Node>> | undefined;
+  } = {
+    ClassDeclaration: (node => ({
+      displayInfo: {
+        priority: DisplayInfoPriority.MEDIUM,
+        label: [{ text: "class", style: LabelStyle.SECTION_NAME }],
+      },
+    })) as Enchancer<Node<ts.ClassDeclaration>>,
+  };
   `;
-  //   file = `
-  // import {
-  //   Node,
-  //   DisplayInfoPriority,
-  //   LabelStyle,
-  //   DisplayInfo,
-  // } from "../../tree/node";
-  // import * as ts from "typescript";
-  // export type Enchancer<T extends Node<ts.Node>> = (
-  //   node: T,
-  // ) => {
-  //   displayInfo: DisplayInfo;
-  // };
-  // export const enchancers: {
-  //   [key: string]: Enchancer<Node<ts.Node>> | undefined;
-  // } = {
-  //   ClassDeclaration: (node => ({
-  //     displayInfo: {
-  //       priority: DisplayInfoPriority.MEDIUM,
-  //       label: [{ text: "class", style: LabelStyle.SECTION_NAME }],
-  //     },
-  //   })) as Enchancer<Node<ts.ClassDeclaration>>,
-  // };
-  // `;
 
   loadTree(filePath: string): Node<WorkingSet> {
     return RootNode.fromState({
