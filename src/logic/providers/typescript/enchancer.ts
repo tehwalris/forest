300fc35141c0e921c089d3543ef83dae08385eca
Philippe Voinov <philippevoinov@gmail.com>
2020-03-01 14:39:49 +0100

    Remove useless explicit enchancers

--- src/logic/providers/typescript/enchancer.ts
+++ src/logic/providers/typescript/enchancer.ts
@@ -35,35 +35,6 @@ export type Enchancer<T extends Node<ts.Node>> = (
 export const enchancers: {
   [key: string]: Enchancer<Node<any>> | undefined;
 } = {
-  ClassDeclaration: (node: Node<ts.ClassDeclaration>) => {
-    const label: LabelPart[] = [
-      { text: "class", style: LabelStyle.SECTION_NAME },
-    ];
-
-    return {
-      displayInfo: {
-        priority: DisplayInfoPriority.MEDIUM,
-        label,
-        color: SemanticColor.DECLARATION,
-      },
-    };
-  },
-  FunctionDeclaration: (node: Node<ts.FunctionDeclaration>) => {
-    const label: LabelPart[] = [
-      { text: "function", style: LabelStyle.SECTION_NAME },
-    ];
-    const name = tryExtractName(node);
-    if (name !== undefined) {
-      label.push({ text: name, style: LabelStyle.NAME });
-    }
-    return {
-      displayInfo: {
-        priority: DisplayInfoPriority.MEDIUM,
-        label,
-        color: SemanticColor.DECLARATION,
-      },
-    };
-  },
   Identifier: (node: Node<ts.Identifier>, parentPath) => {
     const lastParentEntry = R.last(parentPath);
     const isDeclarationName = lastParentEntry?.childKey === "name"; // HACK Clearly we need a more robust detection strategy
