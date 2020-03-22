89e510b3559bb180c327c83d84d483c4df5273fd
Philippe Voinov <philippevoinov@gmail.com>
2020-03-22 15:00:07 +0100

    Don't display numeric child keys

--- src/components/tree/node-content.tsx
+++ src/components/tree/node-content.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 import * as R from "ramda";
 import { LabelStyle } from "../../logic/tree/node";
 import { ParentIndexEntry } from "../../logic/parent-index";
-
 interface Props {
   parentIndexEntry: ParentIndexEntry | undefined;
 }
-
 export const NodeContent: React.FC<Props> = React.memo(
   ({ parentIndexEntry }) => {
     if (!parentIndexEntry) {
@@ -16,9 +14,10 @@ export const NodeContent: React.FC<Props> = React.memo(
     const displayInfo = node.getDisplayInfo(path)?.label || [
       { text: node.getDebugLabel() || "", style: LabelStyle.UNKNOWN },
     ];
+    const childKey = R.last(path)?.childKey;
     return (
       <div>
-        <div>{R.last(path)?.childKey}</div>
+        {childKey && !childKey.match(/^\d+$/) && <div>{childKey}</div>}
         {displayInfo.map((p, i) => (
           <div key={i}>
             {p.style === LabelStyle.NAME ? <i>{p.text}</i> : p.text}
