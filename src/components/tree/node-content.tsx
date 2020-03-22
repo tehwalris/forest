c6efc6e269ab013087af19036b5194499135aae0
Philippe Voinov <philippevoinov@gmail.com>
2020-03-22 15:53:01 +0100

    WIP: Change all text styles

--- src/components/tree/node-content.tsx
+++ src/components/tree/node-content.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 import * as R from "ramda";
-import { LabelStyle } from "../../logic/tree/node";
+import { LabelStyle, LabelPart } from "../../logic/tree/node";
 import { ParentIndexEntry } from "../../logic/parent-index";
+import { css } from "emotion";
 interface Props {
   parentIndexEntry: ParentIndexEntry | undefined;
+}
+const styles = {
+  typeSummaryPart: css`
+    font-weight: 500;
+  `,
+  unknownPart: css`
+    text-decoration: line-through;
+  `,
+  childKey: css`
+    opacity: 0.5;
+  `,
+};
+function renderLabelPart(p: LabelPart) {
+  if (p.style === LabelStyle.NAME || p.style === LabelStyle.VALUE) {
+    return <span>{p.text}</span>;
+  } else if (p.style === LabelStyle.TYPE_SUMMARY) {
+    return <span className={styles.typeSummaryPart}>{p.text}</span>;
+  } else {
+    return <span className={styles.unknownPart}>{p.text}</span>;
+  }
 }
 export const NodeContent: React.FC<Props> = React.memo(
   ({ parentIndexEntry }) => {
@@ -17,11 +38,11 @@ export const NodeContent: React.FC<Props> = React.memo(
     const childKey = R.last(path)?.childKey;
     return (
       <div>
-        {childKey && !childKey.match(/^\d+$/) && <div>{childKey}</div>}
+        {childKey && !childKey.match(/^\d+$/) && (
+          <div className={styles.childKey}>{childKey}</div>
+        )}
         {displayInfo.map((p, i) => (
-          <div key={i}>
-            {p.style === LabelStyle.NAME ? <i>{p.text}</i> : p.text}
-          </div>
+          <div key={i}>{renderLabelPart(p)}</div>
         ))}
       </div>
     );
