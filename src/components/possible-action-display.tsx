767ef0ac567de7f8e26c0949d20908311ae1f6b9
Philippe Voinov <philippevoinov@gmail.com>
2020-03-22 13:30:13 +0100

    Update shortcut help display

--- src/components/possible-action-display.tsx
+++ src/components/possible-action-display.tsx
@@ -1,22 +1,20 @@
 import * as React from "react";
 import { ActionSet } from "../logic/tree/action";
 import { Node } from "../logic/tree/node";
-
 interface Props {
   actions: ActionSet<Node<unknown>>;
 }
 const SHORTCUTS_BY_ACTION_KEY: {
   [key: string]: string | undefined;
 } = {
-  prepend: "r",
-  append: "a",
+  append: "ctrl-right",
   setFromString: "s",
   setVariant: "v",
   toggle: "t",
   insertByKey: "i",
   deleteByKey: "d",
 };
-
+const PARENT_SHORTCUTS = ["replace", "deleteChild", "insertChildAtIndex"];
 export const PossibleActionDisplay: React.FC<Props> = ({ actions }) => (
   <div>
     {Object.keys(actions).length ? (
@@ -24,7 +22,7 @@ export const PossibleActionDisplay: React.FC<Props> = ({ actions }) => (
         Possible actions:{" "}
         {Object.keys(actions)
           .sort()
-          .filter(a => !["replace", "deleteChild"].includes(a))
+          .filter(a => !PARENT_SHORTCUTS.includes(a))
           .map(a => {
             const shortcut = SHORTCUTS_BY_ACTION_KEY[a];
             return shortcut ? `${a} (${shortcut})` : a;
@@ -35,7 +33,7 @@ export const PossibleActionDisplay: React.FC<Props> = ({ actions }) => (
       "The selected node may not support any actions"
     )}
     <br />
-    Other maybe possible actions: delete (x), copy (c), paste (p),
-    printToConsole (0), save (9)
+    Other maybe possible actions: delete (x), add sibling below (ctrl-down), add
+    sibling above (ctrl-up), copy (c), paste (p), printToConsole (0), save (9)
   </div>
 );
