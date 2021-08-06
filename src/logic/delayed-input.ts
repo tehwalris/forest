b62c638bc1cf7a73f1f609c574d17a6ccbe57d30
Philippe Voinov <philippevoinov@gmail.com>
2021-08-06 10:54:13 +0200

    Fix fast keypresses being missed sometimes

--- src/logic/delayed-input.ts
+++ src/logic/delayed-input.ts
@@ -12,26 +12,27 @@ export type DelayedInput =
       kind: DelayedInputKind.KeyDown;
       event: KeyboardEvent;
     };
+
 export function useDelayedInput(
-  handleInput: (input: DelayedInput) => void,
+  _handleInput: (input: DelayedInput) => void,
 ): (input: DelayedInput) => void {
+  // HACK without this ref sometimes an outdated callback is used on fast keypresses
+  const latestHandleInputRef = useRef(_handleInput);
+  latestHandleInputRef.current = _handleInput;
   const [queue, setQueue] = useState<DelayedInput[]>([]);
   const queueEmptyRef = useRef(false);
   queueEmptyRef.current = !queue.length;
   const immediatelyHandledThisRenderRef = useRef(false);
   immediatelyHandledThisRenderRef.current = false;
-  const queueInput = useCallback(
-    (input: DelayedInput) => {
-      if (queueEmptyRef.current && !immediatelyHandledThisRenderRef.current) {
-        immediatelyHandledThisRenderRef.current = true;
-        handleInput(input);
-      } else {
-        queueEmptyRef.current = false;
-        setQueue((oldQueue) => [...oldQueue, input]);
-      }
-    },
-    [handleInput],
-  );
+  const queueInput = useCallback((input: DelayedInput) => {
+    if (queueEmptyRef.current && !immediatelyHandledThisRenderRef.current) {
+      immediatelyHandledThisRenderRef.current = true;
+      latestHandleInputRef.current(input);
+    } else {
+      queueEmptyRef.current = false;
+      setQueue((oldQueue) => [...oldQueue, input]);
+    }
+  }, []);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (!queue.length) {
@@ -39,7 +40,7 @@ export function useDelayedInput(
     }
     const input = queue[0];
     setQueue((oldQueue) => oldQueue.slice(1));
-    handleInput(input);
+    latestHandleInputRef.current(input);
   });
   return queueInput;
 }
