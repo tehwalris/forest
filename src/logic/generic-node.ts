8b94d8f6775e1eabc091c6fe983f0bb55246612f
Philippe Voinov <philippevoinov@gmail.com>
2021-10-25 17:45:52 +0200

    WIP: InterfaceDeclaration (heritageClauses are broken)

--- src/logic/generic-node.ts
+++ src/logic/generic-node.ts
@@ -18,4 +18,5 @@ export const allowedGenericNodeMatchers: ((node: ts.Node) => boolean)[] = [
   ts.isForInStatement,
   ts.isParameter,
   ts.isTypeAliasDeclaration,
+  ts.isInterfaceDeclaration,
 ];
