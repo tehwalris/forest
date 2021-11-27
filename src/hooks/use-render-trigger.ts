355ebccf366d47dc7d363e60c124f7df387ecaf0
Philippe Voinov <philippevoinov@gmail.com>
2021-11-27 23:29:50 +0100

    Fix lint warning

--- src/hooks/use-render-trigger.ts
+++ src/hooks/use-render-trigger.ts
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 
 export function useRenderTrigger(): () => void {
-  const [_dummy, setDummy] = useState({});
+  const [, setDummy] = useState({});
   const triggerRender = useCallback(() => {
     setDummy({});
   }, []);
