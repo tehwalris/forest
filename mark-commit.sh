65359043be215f86f49bc2f85b82fda020468658
Philippe Voinov <philippevoinov@gmail.com>
2021-12-22 15:00:43 +0100

    WIP: Start marking interesting commits

---
+++ mark-commit.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <output file> <initial offset>"
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    touch "$1"
+fi
+
+offset=$2
+while true; do
+  commit_offset_rev="HEAD~$offset"
+  commit_hash=$(git rev-parse "$commit_offset_rev")
+  if [ $? -ne 0 ]; then
+    exit 0
+  fi
+
+  clear
+  git show "$commit_hash"
+  read -p "Was that commit ($commit_offset_rev) interesting?" -n 1 -r
+  echo "$commit_hash" "$REPLY" >> $1
+
+  offset=$((offset+1))
+done
