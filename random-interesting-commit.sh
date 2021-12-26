dd645015f8e6315b7b85f43a6dde57f60760a027
Philippe Voinov <philippevoinov@gmail.com>
2021-12-26 22:44:11 +0100

    Actually only use the interesting commits

--- random-interesting-commit.sh
+++ random-interesting-commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-cat doc/interesting-commits.txt | choose 0 | sort -R | GIT_PAGER="less -+F -RSX" xargs -IHASH bash -c 'git show HASH || true'
+cat doc/interesting-commits.txt | grep -E 'y$' | choose 0 | sort -R | GIT_PAGER="less -+F -RSX" xargs -IHASH bash -c 'git show HASH || true'
