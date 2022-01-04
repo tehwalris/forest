4171a1a29131f09dfbb91e079ced3ba9df991735
Philippe Voinov <philippevoinov@gmail.com>
2022-01-04 18:06:37 +0100

    Delete tasks which use non-standard syntax

--- tasks/editing/cpojer-js-codemod-flow-bool-to-boolean.after.ts
+++
@@ -1,7 +0,0 @@
-
-
-var a: boolean = 10;
-var b: boolean = 10;
-function c<T: boolean>(x: boolean): boolean {
-  
-}
--- tasks/editing/cpojer-js-codemod-flow-bool-to-boolean.before.ts
+++
@@ -1,7 +0,0 @@
-
-
-var a: bool = 10;
-var b: boolean = 10;
-function c<T: bool>(x: bool): bool {
-  
-}
