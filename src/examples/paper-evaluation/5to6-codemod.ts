6651122d59aed8810181bf2c96ebdf00125750cc
Philippe Voinov <philippevoinov@gmail.com>
2022-09-08 22:25:43 +0200

    Add examples for 5to6-codemod

--- src/examples/paper-evaluation/5to6-codemod.ts
+++ src/examples/paper-evaluation/5to6-codemod.ts
@@ -35,4 +35,93 @@ export const examples5to6Codemod: Example[] = [
       },
     ],
   },
+  {
+    nameParts: ["paper-evaluation", "5to6-codemod", "no-strict"],
+    describedGroups: [
+      {
+        description: 'Search for "use strict"',
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for ExpressionStatement with string literal",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode &&
+                    ts.isExpressionStatement(node.tsNode) &&
+                    ts.isStringLiteral(node.tsNode.expression) &&
+                    node.tsNode.expression.text === "use strict",
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+        ],
+      },
+      {
+        description: "Delete the statement",
+        eventCreators: [fromKeys("d")],
+      },
+    ],
+  },
+  {
+    nameParts: ["paper-evaluation", "5to6-codemod", "simple-arrow"],
+    describedGroups: [
+      {
+        description: "Search for functions with only return in body",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for functions with only return in body",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode &&
+                    ts.isFunctionExpression(node.tsNode) &&
+                    ts.isBlock(node.tsNode.body) &&
+                    node.tsNode.body.statements.length === 1 &&
+                    ts.isReturnStatement(node.tsNode.body.statements[0]),
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+        ],
+      },
+      {
+        description: "Set a mark on the original function",
+        eventCreators: [fromKeys("m a")],
+      },
+      {
+        description: "Insert an arrow function and set a mark",
+        eventCreators: [
+          fromKeys("{ i"),
+          toTypeString("()=>x;"),
+          fromKeys("escape m b"),
+        ],
+      },
+      {
+        description: "Copy the expression from the returned statement",
+        eventCreators: [fromKeys("shift-l space alt-l c")],
+      },
+      {
+        description: "Paste the expression into the arrow function",
+        eventCreators: [fromKeys("shift-m b alt-l p")],
+      },
+      {
+        description: "Copy the arguments from the original function",
+        eventCreators: [fromKeys("shift-m a ( c")],
+      },
+      {
+        description: "Paste the arguments into the arrow function",
+        eventCreators: [fromKeys("shift-m b ( p")],
+      },
+      {
+        description: "Replace the function with the arrow function",
+        eventCreators: [fromKeys("k k c } k p")],
+      },
+    ],
+  },
 ];
