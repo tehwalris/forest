c0d9cbeb315963a2388f80f6b0f6653b830ecba4
Philippe Voinov <philippevoinov@gmail.com>
2022-09-10 10:24:55 +0200

    js-codemod/arrow-function example fully working

--- src/examples/paper-evaluation/js-codemod.ts
+++ src/examples/paper-evaluation/js-codemod.ts
@@ -81,6 +81,25 @@ export const examplesJsCodemod: Example[] = [
         ],
       },
       {
+        description: "Delete cursors on functions which have a name",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for identifiers",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode && ts.isIdentifier(node.tsNode),
+                },
+                { shallowSearchForRoot: true },
+              ),
+          },
+          fromKeys("shift-y f"),
+        ],
+      },
+      {
         description: "Insert an arrow function and set a mark",
         eventCreators: [
           fromKeys("k { i"),
@@ -103,6 +122,232 @@ export const examplesJsCodemod: Example[] = [
       {
         description: "Replace the function with the arrow function",
         eventCreators: [fromKeys("k k c } k k p")],
+      },
+      {
+        description: "Reduce to first cursor and select whole file",
+        eventCreators: [fromKeys("shift-s h k k k")],
+      },
+      {
+        description: "Search for function expressions",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for function expressions",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode && ts.isFunctionExpression(node.tsNode),
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+        ],
+      },
+      {
+        description: "Delete cursors which contain this",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for ThisExpression",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    node.tsNode?.kind === ts.SyntaxKind.ThisKeyword,
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+          fromKeys("shift-y f"),
+        ],
+      },
+      {
+        description: "Set a mark on the original function",
+        eventCreators: [fromKeys("m a")],
+      },
+      {
+        description: "Copy the body of the original function",
+        eventCreators: [fromKeys("{ } c k")],
+      },
+      {
+        description:
+          "Select place where generator functions have their asterisk",
+        eventCreators: [fromKeys("alt-h shift-l space")],
+      },
+      {
+        description: "Delete cursors which have an asterisk selected",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for asterisk tokens",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) => isToken(node, ts.isAsteriskToken),
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+          fromKeys("shift-y f"),
+        ],
+      },
+      {
+        description: "Delete cursors on functions which have a name",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for identifiers",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode && ts.isIdentifier(node.tsNode),
+                },
+                { shallowSearchForRoot: true },
+              ),
+          },
+          fromKeys("shift-y f"),
+        ],
+      },
+      {
+        description: "Delete cursors where function is used with bind",
+        eventCreators: [
+          fromKeys("k shift-l space"),
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for the identifier bind",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode &&
+                    ts.isIdentifier(node.tsNode) &&
+                    node.tsNode.text === "bind",
+                },
+                { shallowSearchForRoot: true },
+              ),
+          },
+          fromKeys("shift-y f"),
+        ],
+      },
+      {
+        description: "Insert an arrow function and set a mark",
+        eventCreators: [
+          fromKeys("shift-m a { i"),
+          toTypeString("()=>x;"),
+          fromKeys("escape m b"),
+        ],
+      },
+      {
+        description: "Paste the body into the arrow function",
+        eventCreators: [fromKeys("alt-l p")],
+      },
+      {
+        description: "Copy the arguments from the original function",
+        eventCreators: [fromKeys("shift-m a ( c")],
+      },
+      {
+        description: "Paste the arguments into the arrow function",
+        eventCreators: [fromKeys("shift-m b ( p")],
+      },
+      {
+        description: "Replace the function with the arrow function",
+        eventCreators: [fromKeys("k k c } k p")],
+      },
+      {
+        description: "Reduce to first cursor and select whole file",
+        eventCreators: [fromKeys("shift-s h k k k")],
+      },
+      {
+        description:
+          "Search for arrow functions containing only a return statement",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for arrow functions containing only a return statement",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode &&
+                    ts.isArrowFunction(node.tsNode) &&
+                    ts.isBlock(node.tsNode.body) &&
+                    node.tsNode.body.statements.length === 1 &&
+                    ts.isReturnStatement(node.tsNode.body.statements[0]),
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+        ],
+      },
+      {
+        description: "Replace block by parenthesized return value",
+        eventCreators: [
+          fromKeys("{ alt-l c } d a"),
+          toTypeString("(x)"),
+          fromKeys("escape ( p"),
+        ],
+      },
+      {
+        description: "Delete cursors which are object literals",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for object literals",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode && ts.isObjectLiteralExpression(node.tsNode),
+                },
+                { shallowSearchForRoot: true },
+              ),
+          },
+          fromKeys("shift-y f"),
+        ],
+      },
+      {
+        description: "Remove parentheses",
+        eventCreators: [fromKeys("c ) p")],
+      },
+      {
+        description: "Reduce to first cursor and select whole file",
+        eventCreators: [fromKeys("shift-s h k k k k")],
+      },
+      {
+        description:
+          "Search for arrow functions containing only one ExpressionStatement",
+        eventCreators: [
+          {
+            kind: EventCreatorKind.Function,
+            description:
+              "Use structural search UI (not shown) to search for arrow functions containing only one ExpressionStatement",
+            function: (docManager: DocManager) =>
+              docManager.search(
+                {
+                  match: (node) =>
+                    !!node.tsNode &&
+                    ts.isArrowFunction(node.tsNode) &&
+                    ts.isBlock(node.tsNode.body) &&
+                    node.tsNode.body.statements.length === 1 &&
+                    ts.isExpressionStatement(node.tsNode.body.statements[0]),
+                },
+                { shallowSearchForRoot: false },
+              ),
+          },
+        ],
+      },
+      {
+        description: "Replace arrow function body by expression",
+        eventCreators: [fromKeys("{ c } p")],
       },
     ],
   },
